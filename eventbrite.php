1268500f0d20747a2185fc5e54ee5c424fc21b95
Jamie Hollern <jamiehollern@gmail.com>
2014-02-28 18:31:53 +0000

    Renamed class file.

---
+++ eventbrite.php
@@ -0,0 +1,45 @@
+<?php
+
+  class eventbrite {
+
+    var $endpoint = 'https://www.eventbriteapi.com/v3/';
+    var $token = '';
+    
+    function __construct($oauth_token) {
+      $this->token = $oauth_token;
+    }
+    
+    function __call($method, $args) {
+      // Get the URI we need.
+      $uri = $this->build_uri($method, $args);
+      // Construct the full URL.
+      $request_url = $this->endpoint . $uri;
+      // This array is used to authenticate our request.
+      $options = array(
+        'http' => array(
+          'method' => 'GET',
+          'header'=> "Authorization: Bearer " . $this->token
+        )
+      );
+      // Call the URL and get the data.
+      $resp = file_get_contents($request_url, false, stream_context_create($options));
+      // Return it as arrays/objects.
+      return json_decode($resp);
+    }
+    
+    function build_uri($method, $args) {
+      // Get variables from the $args.
+      extract($args[0]);
+      // Get rid of the args array.
+      unset($args);
+      // Create an array of all the vars within this function scope.
+      // This should be at most 'method', 'id' and 'data'.
+      $vars = get_defined_vars();
+      // Put them together with a slash.
+      $uri = implode($vars, '/');
+      return $uri;
+    }
+
+  }
+
+?>
