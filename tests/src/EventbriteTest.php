ac990135e962fe46f8cb4af2d26c566ec257b5bf
Jamie Hollern <jamiehollern@gmail.com>
2016-04-23 20:30:18 +0100

    Created a dummy test.

---
+++ tests/src/EventbriteTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ * Unit tests for the Eventbrite class.
+ */
+namespace JamieHollern\Eventbrite\Tests;
+
+use JamieHollern\Eventbrite\Eventbrite;
+use Mockery\Mock as m;
+
+/**
+ * Class EventbriteTest
+ *
+ * @package JamieHollern\Eventbrite\Tests
+ */
+class EventbriteTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testTest() {
+        $this->assertEmpty([]);
+    }
+
+}
